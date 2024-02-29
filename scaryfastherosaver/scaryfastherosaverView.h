2be429e8e2e3b16d2555b8eaabf6d39fb21275fc
Greyland Bauer <greylandbauer@gmail.com>
2024-02-28 19:31:57 -0500

    Initial Commit

---
+++ scaryfastherosaver/scaryfastherosaverView.h
@@ -0,0 +1,12 @@
+//
+//  scaryfastherosaverView.h
+//  scaryfastherosaver
+//
+//  Created by Greyland on 2/28/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface scaryfastherosaverView : ScreenSaverView
+
+@end
---
+++ scaryfastherosaver/scaryfastherosaverView.m
@@ -0,0 +1,51 @@
+//
+//  scaryfastherosaverView.m
+//  scaryfastherosaver
+//
+//  Created by Greyland on 2/28/24.
+//
+
+#import "scaryfastherosaverView.h"
+
+@implementation scaryfastherosaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
